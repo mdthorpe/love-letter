ed3e24ecf9a85a5f75c4415aec2da5f3211c4987
Mike Thorpe <mthorpe@unbounce.com>
2015-12-18 16:22:26 -0800

    New font. Common app.js

--- public/js/player.js
+++ public/js/player.js
@@ -1,13 +1,5 @@
-var socket = io();
-
 $('#playername').submit(function(){
   socket.emit('addplayer', $('#p').val());
   $('#topbar').html($('#p').val());
   return false;
-});
-
-// handle incomming messages
-socket.on('statusmessage', function(player, msg) {
-  var txt = player + ': ' + msg;
-  $("#messages").append($('<li>').text(txt));
 });
