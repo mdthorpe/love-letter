c23d321f0fb66f28f7d7f040f9cda108eff243a7
Mike Thorpe <mthorpe@unbounce.com>
2015-12-18 16:24:38 -0800

    Renamed main js

---
+++ love-letter.js
@@ -0,0 +1,76 @@
+var express = require('express');
+    app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    path = require('path');
+
+var PLAYERS = {},
+    HOST = {},
+    ROOM = 'AAAA';
+    PORT = 3000;
+
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+
+app.get('/host', function (req, res) {
+  res.render('host', { room: ROOM, 
+                       title: 'host: ' + ROOM + ' | Love Letter' });
+});
+
+app.get('/player', function (req, res) {
+  res.render('player', { room: ROOM,
+                         title: 'player: ' + ROOM + ' | Love Letter' }); 
+});
+
+app.get('/', function (req, res){
+  res.redirect('/host');
+});
+
+io.on('connection', function (socket) {
+
+    // when the client emits 'addplayer', this listens and executes
+    socket.on('addplayer', function(playername){
+				console.log('addplayer: ' + playername);
+				console.log('room: ' + ROOM);
+
+        socket.username = playername;
+        socket.room = ROOM
+        PLAYERS[socket.id] = playername;
+
+        socket.join(ROOM);
+        // echo to client they've connected
+        socket.emit('statusmessage', 'SERVER', 'you have connected to ' + ROOM);
+        // echo to room that a person has connected to their room
+        socket.broadcast.to(ROOM).emit('statusmessage', 'SERVER', playername + ' has connected to this room');
+        console.log(PLAYERS);
+        console.log(HOST);
+        socket.broadcast.to(HOST).emit('playerlist', PLAYERS);
+    });
+
+    // when the client emits 'addhost', this listens and executes
+    socket.on('addhost', function(callback){
+        console.log('adding host: ' + socket.id);
+        console.log('room: ' + ROOM);
+        
+        socket.username = 'HOST';
+        socket.room = ROOM;
+
+        HOST = socket.id;
+
+        socket.join(ROOM);
+        socket.broadcast.to(ROOM).emit('statusmessage', 'SERVER', socket.username + ' has connected to this room');
+
+        callback('Host is ready!');
+    });
+
+  socket.on('disconnect', function(){
+    delete PLAYERS[socket.id];
+    socket.broadcast.to(HOST).emit('playerlist', PLAYERS);
+  });
+
+});
+
+http.listen(PORT, function(){
+  console.log('listening on *:' + PORT);
+});
+
