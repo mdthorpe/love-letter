484a11e71fac4dc64f9e2feee1b05bd410c7c342
Mike Thorpe <mt@foo.ca>
2016-03-18 13:08:42 -0700

    Clean up whitespace

--- app.js
+++ app.js
@@ -41,18 +41,18 @@ app.use(express.static('public'));
 app.use(request_logger);
 
 app.get('/host', function (req, res) {
-  res.render('host', { room: ROOM, 
+  res.render('host', { room: ROOM,
      title: 'host:' + ROOM,
      total_players: NUM_PLAYERS });
 });
 
 app.get('/player', function (req, res) {
   res.render('player', { room: ROOM,
-   title: 'player: ' + ROOM }); 
+   title: 'player: ' + ROOM });
 });
 
 app.get('/events', function (req, res) {
-  res.render('events'); 
+  res.render('events');
 });
 
 app.get('/', function (req, res){
@@ -66,7 +66,7 @@ io.on('connection', function (socket) {
             socket.broadcast.to(ROOM).emit('event', msg);
         }
     };
-    
+
     socket.on('*', function (msg) {
         event_stream({ "source": 'event',
                        "recieved" : msg});
@@ -95,23 +95,23 @@ io.on('connection', function (socket) {
         // join the room channel
         socket.room = ROOM;
         socket.join(ROOM);
-        event_stream({ "source": 'player', 
+        event_stream({ "source": 'player',
                        "connect": playername});
 
-        broadcast_player_list(); 
+        broadcast_player_list();
 
-        callback(true);  
-        return false;   
+        callback(true);
+        return false;
     });
 
     // when the client emits 'addhost', this listens and executes
     socket.on('addhost', function(callback){
         HOSTS[socket.id] = socket.id;
-        
+
         socket.room = ROOM;
         socket.join(ROOM);
 
-        event_stream({ "source": 'host', 
+        event_stream({ "source": 'host',
                        "connect": socket.id});
 
         broadcast_player_list();
@@ -125,36 +125,36 @@ io.on('connection', function (socket) {
 
         socket.room = ROOM;
         socket.join(ROOM);
-        
+
         // echo to room that a person has connected to their room
-        event_stream({ "source": 'event_listener', 
+        event_stream({ "source": 'event_listener',
                        "event": socket.id + ' connected.'});
 
         callback(true);
     });
 
     socket.on('disconnect', function(){
-        if (PLAYERS[socket.username]){ 
-            event_stream({ "source": 'socket', 
+        if (PLAYERS[socket.username]){
+            event_stream({ "source": 'socket',
              "event" : 'remove player: ' + PLAYERS[socket.username]});
 
-            delete PLAYERS[socket.username]; 
+            delete PLAYERS[socket.username];
             broadcast_player_list();
         }
-        if (HOSTS[socket.id]){ 
-            event_stream({ "source": 'socket', 
+        if (HOSTS[socket.id]){
+            event_stream({ "source": 'socket',
              "event" : 'remove host: ' + HOSTS[socket.id]});
 
-            delete HOSTS[socket.id]; 
+            delete HOSTS[socket.id];
         }
     });
 
   // Player Events
   //
-  socket.on('player-ready', function(callback){    
+  socket.on('player-ready', function(callback){
     // echo to room that a person has connected to their room
     console.log(socket.username + ' ready.');
-    event_stream({ "source": 'player', 
+    event_stream({ "source": 'player',
                    "event": socket.username + ' ready.'});
     PLAYERS[socket.username].ready = true;
     broadcast_player_list();
